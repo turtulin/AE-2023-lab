ddeabc0f167f3b840991da73f2b127fb41a5cd22
Marta Musso <mm.martamusso@gmail.com>
2023-05-20 00:06:12 +0200

    Version with nested for loop but wrong values

--- sketch.ino
+++ sketch.ino
@@ -1,26 +1,22 @@
 #define DHT22_PIN 2
 
-#include "Arduino.h"
-
 float temperature = 0;
 float humidity = 0;
 
 void setup() {
   Serial.begin(9600);
+  DDRD &= ~(1 << DHT22_PIN);
+  PORTD |= (1 << DHT22_PIN); // Enable pull-up resistor
+  delayMicroseconds(2000); // Delay to allow the sensor to stabilize
 }
 
 void loop() {
-  delay(2000);
-  readDHT22(DHT22_PIN);
-  delay(2000);
+  delay(6000);
   readDHT22(DHT22_PIN);
 }
 
+
 void readDHT22(uint8_t pin) {
-  uint8_t data[5] = {0, 0, 0, 0, 0};
-  uint8_t bitIndex = 7;
-  uint8_t byteIndex = 0;
-  
   DDRD |= (1 << pin);
   PORTD &= ~(1 << pin);
   delayMicroseconds(1000);
@@ -39,44 +35,37 @@ void readDHT22(uint8_t pin) {
     Serial.println("No response from sensor");
     return;
   }
-  
-  for(int i = 0; i < (40); i++) {
-    while (!(PIND & (1 << pin)));
-    uint32_t startMicros = micros();
-    while (PIND & (1 << pin));
-    uint16_t durationHigh = micros() - startMicros;
 
-    if(durationHigh > 48) {
-      data[byteIndex] |= (1 << bitIndex);
-    }
+  // Read the DHT22 sensor data
+  uint8_t data[5] = {0, 0, 0, 0, 0};
 
-    if(bitIndex == 0) {
-      bitIndex = 7;
-      byteIndex++;
-    }
-    else {
-      bitIndex--;
+  for (int i = 0; i < 5; i++) {
+    for (int j = 7; j >= 0; j--) {
+      while (!(PIND & (1 << pin)));
+      delayMicroseconds(30);
+      if (PIND & (1 << pin)) {
+        data[i] |= (1 << j);
+      }
+      delayMicroseconds(48);
     }
   }
-   
-   uint8_t checksum = data[0] + data[1] + data[2] + data[3];
-   
-   if (checksum != data[4]) {
-     Serial.println("Checksum error");
-     return;
-   }
-   
-   humidity = ((data[0] << 8) | data[1]) / 10.0;
-   temperature = (((data[2] & 0x7F) << 8) | data[3]) / 10.0;
-   
-   if(data[2] & 0x80)
-   temperature = -temperature;
 
-   
-   Serial.print("Humidity: ");
-   Serial.print(humidity);
-   Serial.print("%\t");
-   Serial.print("Temperature: ");
-   Serial.print(temperature);
-   Serial.println("°C");
+  uint8_t checksum = data[0] + data[1] + data[2] + data[3];
+  if (checksum != data[4]) {
+    Serial.println("Checksum error");
+  }
+
+  humidity = ((data[0] << 8) + data[1]) / 10.0;
+  temperature = (((data[2] & 0x7F) << 8) + data[3]) / 10.0;
+  if (data[2] & 0x80) {
+    temperature = -temperature;
+  }
+
+  Serial.println(humidity);
+  Serial.println(temperature);
+  Serial.println(data[0]);
+  Serial.println(data[1]);
+  Serial.println(data[2]);
+  Serial.println(data[3]);
+  Serial.println(data[4]);
 }
