6483d587c431f30aeee4193104d389fe335aa5f7
Marta Musso <mm.martamusso@gmail.com>
2023-05-21 18:55:42 +0200

    added myPortInputRegister based on documentation about pgm_read_word()

--- sketch.ino
+++ sketch.ino
@@ -1,21 +1,35 @@
+/*
+#define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
+#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
+#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
+#define FRACT_MAX (1000 >> 3)
+
+extern volatile unsigned long timer0_overflow_count;
+extern volatile unsigned long timer0_millis;
+unsigned char timer0_fract;
+*/
+
 #include "wiring_private.h"
 
 #define DHT22_PIN 2
 #define myDigitalWrite(n,level) (level == HIGH? (PORTD |= myBit) : (PORTD &= ~myBit))
 #define myPinMode(n,mode) (mode == OUTPUT? (DDRD |= myBit) : (DDRD &= ~myBit)); \ 
 (mode == INPUT_PULLUP? (PORTD |= (1<<n)) : (NULL))
+#define myPortInputRegister(port) ( (volatile uint8_t *)( __LPM_word_enhanced__( port_to_input_PGM + (port))) )
 
 float temperature = 0;
 float humidity = 0;
+//uint32_t myMillis = 0;
 uint8_t myBit = (1 << DHT22_PIN);
 
 int myDigitalRead(uint8_t pin) {
-	if (*portInputRegister(PORTD) & myBit) return HIGH;
+	if (*myPortInputRegister(PORTD) & myBit) return HIGH;
 	return LOW;
 }
 
 void setup() {
   Serial.begin(9600);
+  //TIMSK0 = 0x4;
 }
 
 void loop() {
@@ -38,6 +52,8 @@ void readDHT22(uint8_t pin) {
   while(!myDigitalRead(pin));         
   while(myDigitalRead(pin));      
 
+  //alternativa al for
+  cli();
   for(uint8_t i=0; i<5; i++) {
     for(uint8_t j=0; j<8; j++) {
       while(!myDigitalRead(DHT22_PIN));      
@@ -49,13 +65,18 @@ void readDHT22(uint8_t pin) {
       while(myDigitalRead(DHT22_PIN));   
     }
   }
+  sei();
 
-  humidity  = ((data[0] << 8) | data[1])/10.0;
-  temperature = ((data[2] & 0x7F) << 8 | data[3])/10.0;
-  if (data[2] & 0x80) {
-    temperature = -temperature;
+  if(data[4] == ((data[0] + data[1] + data[2] + data[3])&0xFF)) {
+    humidity  = ((data[0] << 8) | data[1])/10.0;
+    temperature = ((data[2] & 0x7F) << 8 | data[3])/10.0;
+    if (data[2] & 0x80) {
+      temperature = -temperature;
+    }
   }
 
-  Serial.println(humidity);
+  Serial.print("Temperature: ");
   Serial.println(temperature);
+  Serial.print("Humidity: ");
+  Serial.println(humidity);
 }
