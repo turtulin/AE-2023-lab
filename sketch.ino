5ee77e8fe6c502b0ee6fc5c52c52621f61deb7f1
Marta Musso <mm.martamusso@gmail.com>
2023-05-24 15:30:32 +0200

    added comments

--- sketch.ino
+++ sketch.ino
@@ -6,9 +6,11 @@
 #define RELAY_PIN 3
 #define RELAY_BIT (1 << RELAY_PIN)
 
+//livello minimo e massimo di umidità del suolo (successivamente inseriti dall'utente)
 #define livMin 30
 #define livMax 70
 
+//passare bitmask come argomento n
 #define myDigitalWrite(n,level) (level == HIGH? (PORTD |= n) : (PORTD &= ~n))
 #define myPinMode(n,mode) (mode == OUTPUT ? (DDRD |= n) : (DDRD &= ~n)); \ 
 (mode == INPUT_PULLUP? (PORTD |= (1 << n)) : (NULL))
@@ -23,6 +25,8 @@ float soilHum;
 void setup() {
 }
 
+//uso delay solo per controllare che le funzioni siano corrette
+//il loop dovrà essere vuoto dopo che verranno gestiti gli interrupt
 void loop() {
   delay(2000);
   myReadDHT22(DHT22_PIN);
@@ -34,21 +38,24 @@ void loop() {
   myDigitalWrite(RELAY_BIT,LOW);
 }
 
-
+//funzione per leggere il DHT22 che dovrà operare in background
 void myReadDHT22(uint8_t pin) {
   uint8_t data[5] = {0, 0, 0, 0, 0};
 
+  //segnale start
   myPinMode(DHT22_BIT, OUTPUT);             
   myDigitalWrite(DHT22_BIT, LOW);           
   delayMicroseconds(1000);           
   myDigitalWrite(DHT22_BIT, HIGH);          
   delayMicroseconds(40);             
 
+  //segnale risposta
   myPinMode(DHT22_BIT, INPUT);              
   while(myDigitalRead(DHT22_BIT));          
   while(!myDigitalRead(DHT22_BIT));         
   while(myDigitalRead(DHT22_BIT));      
 
+  //leggo i 40 bit disabilitando interrupt
   cli();
   for(uint8_t i = 0; i < 5; i++) {
     for(uint8_t j = 0; j < 8; j++) {
@@ -63,6 +70,7 @@ void myReadDHT22(uint8_t pin) {
   }
   sei();
 
+  //se il checksum è corretto, salvo i valori nelle variabili globali
   if(data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
     humidity  = ((data[0] << 8) | data[1]) / 10.0;
     temperature = ((data[2] & 0x7F) << 8 | data[3]) / 10.0;
@@ -72,6 +80,7 @@ void myReadDHT22(uint8_t pin) {
   }
 }
 
+//funzione per leggere il potenziometro che dovrà operare in background
 void myAnalogRead(uint8_t pin)
 {
   ADMUX = (pin & 0xF8) | (pin & 0x07);
