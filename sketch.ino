2f43409940a4a170e833561ec41d5789f413232e
Marta Musso <mm.martamusso@gmail.com>
2023-05-24 17:56:34 +0200

    added TIMER1 to operate functions in background. Loop function is now empty

--- sketch.ino
+++ sketch.ino
@@ -6,11 +6,15 @@
 #define RELAY_PIN 3
 #define RELAY_BIT (1 << RELAY_PIN)
 
-//livello minimo e massimo di umidità del suolo (successivamente inseriti dall'utente)
+// Livello minimo e massimo di umidità del suolo (successivamente inseriti dall'utente)
 #define livMin 30
 #define livMax 70
 
-//passare bitmask come argomento n
+// Intervalli in secondi tra le letture dei sensori (successivamente inseriti dall'utente, almeno una lettura al giorno)
+#define intervalDHT22 7200
+#define intervalPot 3600
+
+// Passo bitmask come argomento n
 #define myDigitalWrite(n,level) (level == HIGH? (PORTD |= n) : (PORTD &= ~n))
 #define myPinMode(n,mode) (mode == OUTPUT ? (DDRD |= n) : (DDRD &= ~n)); \ 
 (mode == INPUT_PULLUP? (PORTD |= (1 << n)) : (NULL))
@@ -18,44 +22,47 @@
 
 bool myDigitalRead(uint8_t n) { if(*myPortInputRegister(PORTD)&n) return 1; return 0; }
 
+volatile uint16_t counterDHT22 = 0;
+volatile uint16_t counterPot = 0;
+volatile uint16_t counterRel = 0;
+
 float temperature;
 float humidity;
 float soilHum;
 
 void setup() {
+  cli(); 
+  TCCR1A = 0; // Azzero il registro di controllo A del timer 1
+  TCCR1B = 0; // Azzero il registro di controllo B del timer 1
+  TCNT1 = 0; // Azzera il registro di conteggio del timer 1
+  OCR1A = 15624; // Registro di confronto per interrupt ogni secondo
+  TCCR1B |= (1 << WGM12); // Abilito la modalità di confronto CTC
+  TCCR1B |= (1 << CS12) | (1 << CS10); // Imposto il prescaler a 1024
+  TIMSK1 |= (1 << OCIE1A); // Abilito l'interrupt di confronto A del timer 1
+  sei();
 }
 
-//uso delay solo per controllare che le funzioni siano corrette
-//il loop dovrà essere vuoto dopo che verranno gestiti gli interrupt
 void loop() {
-  delay(2000);
-  myReadDHT22(DHT22_PIN);
-  delay(1000);
-  myAnalogRead(POT_PIN);
-  delay(1000);
-  myDigitalWrite(RELAY_BIT,HIGH);
-  delay(1000);
-  myDigitalWrite(RELAY_BIT,LOW);
 }
 
-//funzione per leggere il DHT22 che dovrà operare in background
+// Funzione per leggere il DHT22 che opera in background
 void myReadDHT22(uint8_t pin) {
   uint8_t data[5] = {0, 0, 0, 0, 0};
 
-  //segnale start
+  // Segnale start
   myPinMode(DHT22_BIT, OUTPUT);             
   myDigitalWrite(DHT22_BIT, LOW);           
   delayMicroseconds(1000);           
   myDigitalWrite(DHT22_BIT, HIGH);          
   delayMicroseconds(40);             
 
-  //segnale risposta
+  // Segnale risposta
   myPinMode(DHT22_BIT, INPUT);              
   while(myDigitalRead(DHT22_BIT));          
   while(!myDigitalRead(DHT22_BIT));         
   while(myDigitalRead(DHT22_BIT));      
 
-  //leggo i 40 bit disabilitando interrupt
+  // Leggo i 40 bit disabilitando interrupt
   cli();
   for(uint8_t i = 0; i < 5; i++) {
     for(uint8_t j = 0; j < 8; j++) {
@@ -70,7 +77,7 @@ void myReadDHT22(uint8_t pin) {
   }
   sei();
 
-  //se il checksum è corretto, salvo i valori nelle variabili globali
+  // Se il checksum è corretto, salvo i valori nelle variabili globali
   if(data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
     humidity  = ((data[0] << 8) | data[1]) / 10.0;
     temperature = ((data[2] & 0x7F) << 8 | data[3]) / 10.0;
@@ -80,11 +87,25 @@ void myReadDHT22(uint8_t pin) {
   }
 }
 
-//funzione per leggere il potenziometro che dovrà operare in background
+// Funzione per leggere il potenziometro che opera in background
 void myAnalogRead(uint8_t pin)
 {
   ADMUX = (pin & 0xF8) | (pin & 0x07);
   ADCSRA |= (1 << ADSC);
   while (ADCSRA & (1 << ADSC));
   soilHum = ADC * (100.0 / 1023.0);
+}
+
+ISR(TIMER1_COMPA_vect) {
+  counterDHT22++;
+  if(counterDHT22 >= intervalDHT22) {
+    counterDHT22 = 0;
+    myReadDHT22(DHT22_PIN);
+  }
+  
+  counterPot++;
+  if(counterPot >= intervalPot) {
+    counterPot = 0;
+    myAnalogRead(POT_PIN);
+  }  
 }
